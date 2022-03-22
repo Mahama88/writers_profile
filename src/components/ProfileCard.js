23940b5ba788c58c34d29f5cebf3473ae96775bd
Mahama88 <sheikibnrauf88@gmail.com>
2022-03-22 11:12:01 +0000

    react events

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ProfileCard({ writer }) {
+  const [showBio, setShowBio] = useState(false);
+
+  const handleClick = (bioData) => {
+    setShowBio(!showBio);
+  };
   return (
     <div className="card">
-      <img
-        src={`images/${writer.avatar}.png`}
-        height="300px"
-        width="300px"
-        alt={writer.img}
-      />
-
+      <div className="cardContent">
+        {showBio ? (
+          <div className="biowrap">
+            <p className="bio">{writer.bio}</p>
+          </div>
+        ) : (
+          <img
+            src={`images/${writer.avatar}.png`}
+            height="300px"
+            width="300px"
+            alt={writer.img}
+          />
+        )}
+      </div>
       <div className="textGroup">
         <h3>{writer.name}</h3>
         <p>{writer.email}</p>
